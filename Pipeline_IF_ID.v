dd03a79843001ea41b75bdb98745c333fea3d609
manoflute <b02902102@ntu.edu.tw>
2015-12-04 02:31:31 +0800

    IF_ID stall condition added

--- Pipeline_IF_ID.v
+++ Pipeline_IF_ID.v
@@ -17,7 +17,9 @@ output reg[31:0]    pc_add4_o;
 output reg[31:0]    instruction_o;
 
 always @(posedge clk_i) begin
-  pc_add4_o <= pc_add4_i;
-  instruction_o <= instruction_i;
+  if (!hazard_IF_ID_i) begin//if no stall
+    pc_add4_o <= pc_add4_i;
+    instruction_o <= instruction_i;
+  end
 end
 endmodule
