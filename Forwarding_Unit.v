052f3aaaa72b961224faf97f1e48115fbb87bfbb
manoflute <b02902102@ntu.edu.tw>
2015-12-04 02:11:25 +0800

    variable name modified

--- Forwarding_Unit.v
+++ Forwarding_Unit.v
@@ -14,20 +14,20 @@ input       MEM_WB_RegWrite_i;
 input[1:0]  EX_MEM_WB_i;
 
 output[1:0] select_ALU_data1_o, select_ALU_data2_o;
-wire EXHarzard_rs, MEMHarzard_rs, EXHarzard_rt, MEMHarzard_rt;
-assign EXHarzard_rs = EX_MEM_WB_i && 
+wire EXHazard_rs, MEMHazard_rs, EXHazard_rt, MEMHazard_rt;
+assign EXHazard_rs = EX_MEM_WB_i && 
   (EX_MEM_RegDst_i != 5'd0) && 
   (EX_MEM_RegDst_i == ID_EX_RSaddr_i) ;
-assign EXHarzard_rt = EX_MEM_WB_i && 
+assign EXHazard_rt = EX_MEM_WB_i && 
   (EX_MEM_RegDst_i != 5'd0) && 
   (EX_MEM_RegDst_i == ID_EX_RTaddr_i) ;
-assign MEMHarzard_rs = MEM_WB_RegWrite_i &&
+assign MEMHazard_rs = MEM_WB_RegWrite_i &&
   (MEM_WB_RegDst_i != 5'd0) &&
   (MEM_WB_RegDst_i != ID_EX_RSaddr_i);
-assign MEMHarzard_rt = MEM_WB_RegWrite_i &&
+assign MEMHazard_rt = MEM_WB_RegWrite_i &&
   (MEM_WB_RegDst_i != 5'd0) &&
   (MEM_WB_RegDst_i != ID_EX_RTaddr_i);
 
-assign select_ALU_data1_o = (EXHarzard_rs)? 2'b10: (MEMHarzard_rs)? 2'b01: 2'b00;
-assign select_ALU_data2_o = (EXHarzard_rt)? 2'b10: (MEMHarzard_rt)? 2'b01: 2'b00;
+assign select_ALU_data1_o = (EXHazard_rs)? 2'b10: (MEMHazard_rs)? 2'b01: 2'b00;
+assign select_ALU_data2_o = (EXHazard_rt)? 2'b10: (MEMHazard_rt)? 2'b01: 2'b00;
 endmodule
