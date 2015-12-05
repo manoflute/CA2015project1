30c63ce90a0d55efd52dde77a201367bd151fc10
manoflute <b02902102@ntu.edu.tw>
2015-12-05 15:10:25 +0800

    mul funct_i modified

--- ALU_Control.v
+++ ALU_Control.v
@@ -13,8 +13,8 @@ wire[5:0] funct_i;
 assign funct_i = immediate_i[5:0];
 
 wire[2:0] ALU_op_selections[2:0];
-assign ALU_op_selections[2'b00] = 3'b000;
-assign ALU_op_selections[2'b01] = 3'b010;
-assign ALU_op_selections[2'b10] = funct_i[2:0];
-assign ALU_Ctrl_o = ALU_op_selections[ALU_op_i];
+assign ALU_op_selections[2'b00] = 3'b000;       //add
+assign ALU_op_selections[2'b01] = 3'b010;       //sub
+assign ALU_op_selections[2'b10] = funct_i[2:0]; 
+assign ALU_Ctrl_o = (6'b011000 == funct_i)? 3'b111: ALU_op_selections[ALU_op_i];
 endmodule
